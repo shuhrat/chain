d250b35253023a983f3c2398b099cb3028a90f88
Shuhrat Shadmonkulov <shadmonkulov@gmail.com>
2016-01-05 02:37:36 +0300

    temp

---
+++ Gruntfile.js
@@ -0,0 +1,122 @@
+/*jslint node: true */
+"use strict";
+
+
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        bower: {
+            install: {
+                options: {
+                    install: true,
+                    copy: false,
+                    targetDir: './libs',
+                    cleanTargetDir: true
+                }
+            }
+        },
+
+        uglify: {
+            dist: {
+                files: {
+                    'dist/app.js': ['dist/app.js']
+                },
+                options: {
+                    mangle: false
+                }
+            }
+        },
+
+        html2js: {
+            dist: {
+                src: ['app/templates/*.html'],
+                dest: 'tmp/templates.js'
+            }
+        },
+
+        clean: {
+            temp: {
+                src: ['tmp']
+            }
+        },
+
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['app/*.js', 'tmp/*.js'],
+                dest: 'dist/app.js'
+            }
+        },
+
+        jshint: {
+            all: ['Gruntfile.js', 'app/*.js', 'app/**/*.js']
+        },
+
+        connect: {
+            server: {
+                options: {
+                    hostname: 'localhost',
+                    port: 8080
+                }
+            }
+        },
+
+        watch: {
+            dev: {
+                files: ['Gruntfile.js', 'app/*.js', '*.html'],
+                tasks: ['jshint', 'html2js:dist', 'concat:dist', 'clean:temp'],
+                options: {
+                    atBegin: true
+                }
+            },
+            min: {
+                files: ['Gruntfile.js', 'app/*.js', '*.html'],
+                tasks: ['jshint', 'html2js:dist', 'concat:dist', 'clean:temp', 'uglify:dist'],
+                options: {
+                    atBegin: true
+                }
+            }
+        },
+
+        compress: {
+            dist: {
+                options: {
+                    archive: 'dist/<%= pkg.name %>-<%= pkg.version %>.zip'
+                },
+                files: [{
+                    src: ['index.html'],
+                    dest: '/'
+                }, {
+                    src: ['dist/**'],
+                    dest: 'dist/'
+                }, {
+                    src: ['assets/**'],
+                    dest: 'assets/'
+                }, {
+                    src: ['libs/**'],
+                    dest: 'libs/'
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-html2js');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-bower-task');
+
+    grunt.registerTask('dev', ['bower', 'connect:server', 'watch:dev']);
+    grunt.registerTask('test', ['bower', 'jshint' ]);
+    grunt.registerTask('minified', ['bower', 'connect:server', 'watch:min']);
+    grunt.registerTask('package', ['bower', 'jshint', 'html2js:dist', 'concat:dist', 'uglify:dist',
+        'clean:temp', 'compress:dist']);
+};
